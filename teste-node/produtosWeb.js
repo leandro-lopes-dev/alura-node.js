a2ea5656bdf23e1d6ef708b47d54f3d0c1cb8bcc
Leandro Lopes <leandro.lopes.dev@gmail.com>
2018-08-07 15:10:05 -0300

    Trabalhando com endereços diferentes

--- teste-node/produtosWeb.js
+++ teste-node/produtosWeb.js
@@ -1,8 +1,12 @@
 
 var http = require('http');
 
-var server = http.createServer(function(req,res){
+var server = http.createServer(function(request,response){
+    if(request.url == "/produtos"){
     res.end("<html><body><h1>Listando os produtos</h1></body></html>");
+    }else{
+        response.end("<html><body><h1>Você está na Home</h1></body></html>");
+    }
 });
 
 server.listen(3000);
